6f2264993b434565c191e1a5e5255e96af4c37eb
jalakPatoliya <jalakpatoliya@gmail.com>
2023-04-26 18:10:15 +0530

    🚑 food2fork.com replaced with working api

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -9,7 +9,8 @@ export default class Recipe {
 
   async getRecipe() {
     try {
-      const res = await axios(`https://www.food2fork.com/api/get?key=${key[2]}&rId=${this.id}`);
+      // const res = await axios(`https://www.food2fork.com/api/get?key=${key[2]}&rId=${this.id}`);
+      const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
       console.log(res);
       this.title = res.data.recipe.title;
       this.author = res.data.recipe.publisher;
